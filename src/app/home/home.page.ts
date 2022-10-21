dc4e21bb2523220a4dd0dfd8b130f5aa8fb2d3f9
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2022-10-21 00:26:14 -0300

    :rocket: changes dmuration to event close

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,15 +14,23 @@ export class HomePage {
   constructor(private toastController: ToastController) {}
 
   handleCalculator(){
-    const imc = (this.peso / (this.altura * this.altura) ;
+    const imc = this.peso / (this.altura * this.altura );
     //(Math.exp(this.altura)))
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    this.showMessage(`IMC = ${imc.toFixed(2)}`); // String literal for concat label and var
   }
 
   async showMessage(msg : string) {
+
+    const previousToast = await this.toastController.getTop();
+    if(previousToast){
+      await this.toastController.dismiss();
+    }
     const toast = await this.toastController.create({
       message:msg,
-      duration:3000
+      //duration:3000
+      buttons:[{
+        icon:'close'
+      }]
     });
 
     toast.present();
